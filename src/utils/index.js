d55323469d37913e19c6d796e25087139e7a65e8
hypocrisy <conaniskenan@gmail.com>
2021-05-04 14:09:08 +0800

    添加usedebounce

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
 /*
  * @Author: hypocrisy
  * @Date: 2021-05-03 21:24:26
  * @LastEditors: hypocrisy
- * @LastEditTime: 2021-05-03 22:14:06
+ * @LastEditTime: 2021-05-04 13:58:05
  * @FilePath: /mock/src/utils/index.js
  */
+import { useEffect, useState } from 'react'
 export const cleanObject = obj => {
 	const res = { ...obj }
 	Object.keys(obj).forEach(key => {
@@ -14,4 +16,21 @@ export const cleanObject = obj => {
 		}
 	})
 	return res
+}
+export const useMount = callback => {
+	useEffect(() => {
+		callback()
+	}, [])
+}
+export const useDebounce = (value, delay) => {
+	const [debouceValue, setDebouceValue] = useState(value)
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setDebouceValue(value)
+		}, delay)
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [value, delay])
+	return debouceValue
 }
